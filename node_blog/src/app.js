f950dc971f2bea36ae022d47a968d747f293f467
luogeger <xiaoqing1232@163.com>
2017-07-08 18:51:11 +0800

    首页的渲染，

--- node_blog/src/app.js
+++ node_blog/src/app.js
@@ -43,6 +43,36 @@ app.use(express.static(__dirname));
 // app.use( homeRouter);
 
 
+/*  5.
+ *   因为每个 router->controller 都要用到 xtpl,
+ *       简化模板 ``xtpl`` 的调用， -> 这样, controller 里面就可以直接用 res.render()
+ *   app.set('views', './views'); -- 设置模板的路径
+ * */
+const xtpl = require('xtpl');
+app.set('views', './views');
+app.set('view engine', 'html');
+app.engine('html', xtpl.renderFile);
+
+
+/*  6.
+ *   配置orm, 创建对象和表的映射
+ *       - 1.在app.js中国创建连接，导入映射
+ *       - 2.控制器中获取数据
+ *          - app.use()是中间件， 执行以后，req.db , req.models
+ *       - 3.中间件的设置要放在路由加载之前
+ * */
+const orm = require('orm')
+app.use(orm.express('mysql://root:@localhost:3306/my_blog',{
+    define: function (db, models, next){
+        // ---------  这里的代码，放到models/model里面了
+
+        const model = require('./models/model');
+        model(db, models);
+        next();
+    }
+}))
+
+
 /*  4.
 *   简化加载路由的步骤
 *       router里面的文件要加prefix -> 它的作用是区分路由
@@ -57,23 +87,14 @@ files.forEach((item) => {
 })
 
 
-/*  5.
-*   因为每个 router->controller 都要用到 xtpl,
-*       简化模板 ``xtpl`` 的调用， -> 这样, controller 里面就可以直接用 res.render()
-*   app.set('views', './views'); -- 设置模板的路径
+
+/*  7.
+*   配置orm 以后，home/index.html里面的数据换成数据库的数据
 * */
-const xtpl = require('xtpl');
-app.set('views', './views');
-app.set('view engine', 'html');
-app.engine('html', xtpl.renderFile);
 
-
-
-
-
-
-
-
+/*  8.
+*   首页
+* */
 
 
 
--- node_blog/src/controllers/home.js
+++ node_blog/src/controllers/home.js
@@ -2,15 +2,27 @@
 
 let controller = module.exports;
 
-
-
-
-
+/*
+*   1.先去查询数据库
+*   2.在传递给模板
+*   3.在html #each 渲染
+*       - 注意 each() 语法错误
+*   4.时间的转换
+*   5.排序 -> ['id', 'Z']
+*   6.分页 -> {limit: 5}
+*   user_id的子表
+* */
 controller.index = (req, res) => {
-
-    res.render('blog/index', (err, html) => {
+    req.models.post.find({}, {limit: 5} , ['id', 'Z'],  (err, posts) => {
         if(err) throw err;
-        res.send(html);
+
+        posts.forEach((item) => {
+            item.created = item.created.toDateString();
+        })
+        res.render('blog/index', {posts: posts}, (err, html) => {
+            if(err) throw err;
+            res.send(html);
+        })
     })
 
 }
